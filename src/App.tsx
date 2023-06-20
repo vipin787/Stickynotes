07df9577ab2926a9014e79101a67ca41fd036bb6
vipin <cvipin787@gmail.com>
2023-06-20 22:07:42 +0530

    Implement Notes component and note component where we can add delete our notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import { NoteObject } from './models/note';
+
+// components
+import Header from './components/Header';
+import Notes from './components/Notes';
+import CreateNote from './components/CreateNote';
+
+import { Box } from '@mui/material';
 
 function App() {
+
+  const [notes, setNotes] = useState<NoteObject[]>([]);
+
+  useEffect(() => {
+    if (sessionStorage.getItem('notes')) {
+      setNotes(JSON.parse(sessionStorage.getItem('notes') as string));
+    }
+  }, [])
+  
+  const deleteNote = (id: number) => {
+    const updatedNotes = notes.filter(note => note.id !== id);
+    setNotes(updatedNotes); 
+    sessionStorage.setItem('notes', JSON.stringify(updatedNotes));
+  }
+
+  const addNote = (note: NoteObject) => {
+    setNotes([ note, ...notes ]);
+    console.log(sessionStorage);
+    sessionStorage.setItem('notes', JSON.stringify([ note, ...notes ]));
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Box style={{ padding: 20 }}>
+        <CreateNote addNote={addNote} />
+        <Notes notes={notes} deleteNote={deleteNote} />
+      </Box>
+    </>
   );
 }
 
